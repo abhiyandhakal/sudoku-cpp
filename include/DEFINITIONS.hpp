08ac1363d8723e784cc7089b4ad30322b94030b8
Abhiyan Dhakal <abheeyaandhacaal@gmail.com>
2022-11-21 07:47:17 +0545

    includes state ids

---
+++ include/DEFINITIONS.hpp
@@ -0,0 +1,11 @@
+#ifndef DEFINITIONS_HPP
+#define DEFINITIONS_HPP
+
+#define EXIT_ID "exit"
+#define MAIN_MENU_ID "main-menu"
+#define LEVELS_ID "levels"
+#define GAME_ID "game"
+#define HOW_TO_PLAY_ID "how-to-play"
+#define HIGH_SCORE_ID "high-score"
+
+#endif
